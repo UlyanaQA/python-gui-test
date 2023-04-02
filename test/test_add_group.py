765bf4b91fa2cb1fe28b04607ddd6966d76362a2
UlyanaQA <mozgulya@gmail.com>
2023-04-02 13:48:08 +0300

    Занятие 8.2: реализован тест добавления группы (двухуровневая структура)

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,2 +1,10 @@
+from model.group import Group
+
+
 def test_add_group(app):
-    pass
+    old_groups = app.groups.get_group_list()
+    group = Group(name="test group11")
+    app.groups.add_new(group)
+    new_groups = app.groups.get_group_list()
+    old_groups.append(group)
+    assert sorted(old_groups, key=Group.name) == sorted(new_groups, key=Group.name)
